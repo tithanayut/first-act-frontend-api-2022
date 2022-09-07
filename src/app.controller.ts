df55c9c05851a1c90911f08b2be6ef4530286a81
Thanayut T <b@thanayut.in.th>
2022-09-07 15:31:13 +0700

    Add deployment

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get()
+  @ApiExcludeEndpoint()
+  getHello(): string {
+    return 'Hello World! Visit the docs at /docs';
+  }
+}
