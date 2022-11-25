08a73ee80e1b55445f32d058436f08c14119595a
Thanayut T <b@thanayut.in.th>
2022-11-26 00:57:17 +0700

    Redirect / to /docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Redirect } from '@nestjs/common';
 import { ApiExcludeEndpoint } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get()
+  @Redirect('/docs', 301)
   @ApiExcludeEndpoint()
   getHello(): string {
-    return 'Hello World! Visit the docs at /docs';
+    return 'Hello World!';
   }
 }
